8d27d74db46ea9bb9c9a2f691c03673bb7fa919a
chpalmer <chpalmer@deloitte.com>
2016-09-28 09:48:53 -0700

    increase cell height for images

--- src/ios/ELCImagePicker/ELCAssetTablePicker.m
+++ src/ios/ELCImagePicker/ELCAssetTablePicker.m
@@ -38,9 +38,9 @@
 
     NSMutableArray *tempArray = [[NSMutableArray alloc] init];
     self.elcAssets = tempArray;
-	
+
     if (self.immediateReturn) {
-        
+
     } else {
         UIBarButtonItem *doneButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(doneAction:)];
         [self.navigationItem setRightBarButtonItem:doneButtonItem];
@@ -73,14 +73,14 @@
     @autoreleasepool {
 
         [self.assetGroup enumerateAssetsUsingBlock:^(ALAsset *result, NSUInteger index, BOOL *stop) {
-            
+
             if (result == nil) {
                 return;
             }
 
             ELCAsset *elcAsset = [[ELCAsset alloc] initWithAsset:result];
             [elcAsset setParent:self];
-            
+
             BOOL isAssetFiltered = NO;
             if (self.assetPickerFilterDelegate &&
                [self.assetPickerFilterDelegate respondsToSelector:@selector(assetTablePicker:isAssetFilteredOut:)])
@@ -106,16 +106,16 @@
                                               atScrollPosition:UITableViewScrollPositionBottom
                                                       animated:NO];
             }
-            
+
             [self.navigationItem setTitle:self.singleSelection ? @"Pick Photo" : @"Pick Photos"];
         });
     }
 }
 
 - (void)doneAction:(id)sender
-{	
+{
 	NSMutableArray *selectedAssetsImages = [[NSMutableArray alloc] init];
-	    
+
 	for (ELCAsset *elcAsset in self.elcAssets) {
 		if ([elcAsset selected]) {
 			[selectedAssetsImages addObject:[elcAsset asset]];
@@ -181,35 +181,36 @@
 
 // Customize the appearance of table view cells.
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{    
+{
     static NSString *CellIdentifier = @"Cell";
-        
+
     ELCAssetCell *cell = (ELCAssetCell*)[tableView dequeueReusableCellWithIdentifier:CellIdentifier];
 
-    if (cell == nil) {		        
+    if (cell == nil) {
         cell = [[ELCAssetCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
     }
-    
+
     [cell setAssets:[self assetsForIndexPath:indexPath]];
-    
+
     return cell;
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-	return 79;
+  // return 79
+	return 100;
 }
 
 - (int)totalSelectedAssets
 {
     int count = 0;
-    
+
     for (ELCAsset *asset in self.elcAssets) {
 		if (asset.selected) {
-            count++;	
+            count++;
 		}
 	}
-    
+
     return count;
 }
 
